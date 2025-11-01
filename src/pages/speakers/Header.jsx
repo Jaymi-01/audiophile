2ea873145448f85f88b30f4117fd1c7ba5b2ca9a
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-11-01 23:00:07 +0100

    Completed the header section

---
+++ src/pages/speakers/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section className='bg-primary text-white px-21 py-8 md:px-69 md:py-24'>
+        <div className='text-center font-bold text-[28px] tracking-[2px] leading-normal'>SPEAKERS</div>
+    </section>
+  )
+}
+
+export default Header
