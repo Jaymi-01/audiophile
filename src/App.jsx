b81f35e15911f32c29ec07b58d94a93442cb0078
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-11-01 22:45:42 +0100

    Imported the speaker page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header.jsx';
 import Home from './pages/home/Index.jsx';
+import Headphones from './pages/headphones/Index.jsx'
+import Speakers from './pages/speakers/Index.jsx'
 
 const App = () => {
   return (
     <Router>
       <div className='font-texts'>
       <Header />
-      <Routes className='bg-background'>
+      <Routes className='bg-background app'>
           <Route path="/" element={<Home />} />
+          <Route path='/headphones' element={<Headphones/>} />
+          <Route path='/speakers' element={<Speakers />} />
           {/* <Route path="/about" element={<About />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/career" element={<Career />} /> */}
