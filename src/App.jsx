dd4fa01f447854d49d760607307827565f2956da
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-11-01 23:06:12 +0100

    Imported the Earphones page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header.jsx';
 import Home from './pages/home/Index.jsx';
 import Headphones from './pages/headphones/Index.jsx'
 import Speakers from './pages/speakers/Index.jsx'
+import Earphones from './pages/earphones/Index.jsx'
 
 const App = () => {
   return (
@@ -14,9 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path='/headphones' element={<Headphones/>} />
           <Route path='/speakers' element={<Speakers />} />
-          {/* <Route path="/about" element={<About />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/career" element={<Career />} /> */}
+          <Route path='/earphones' element={<Earphones />} />
         </Routes>
     </div>
     </Router>
