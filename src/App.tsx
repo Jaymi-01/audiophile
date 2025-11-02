3991198d9d31c67b2d1494e705cc948bbb9e4390
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-11-02 18:30:49 +0100

    added the speaker and removed the extensions from the names

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
-import Header from "./components/Header.jsx";
-import Home from "./pages/home/Index.jsx";
-import Headphones from "./pages/headphones/Index.jsx";
-import Speakers from "./pages/speakers/Index.jsx";
-import Earphones from "./pages/earphones/Index.jsx";
-import XX99MarkII from "./pages/headphones/XX99MarkII.jsx";
-import XX99MarkI from "./pages/headphones/XX99MarkI.js";
+import Header from "./components/Header";
+import Home from "./pages/home/Index";
+import Headphones from "./pages/headphones/Index";
+import Speakers from "./pages/speakers/Index";
+import Earphones from "./pages/earphones/Index";
+import XX99MarkII from "./pages/headphones/XX99MarkII";
+import XX99MarkI from "./pages/headphones/XX99MarkI";
 import XX59 from "./pages/headphones/XX59";
+import ZX9Speaker from "./pages/speakers/ZX9Speaker";
 
 const App: React.FC = () => {
   const cartCount = 3;
@@ -26,6 +27,7 @@ const App: React.FC = () => {
             <Route path="/headphones/xx99-mark-ii" element={<XX99MarkII />} />
             <Route path="/headphones/xx99-mark-i" element={<XX99MarkI />} />
             <Route path="/headphones/xx59" element={<XX59 />} />
+            <Route path="/speakers/zx9" element={<ZX9Speaker />} />
           </Routes>
         </div>
       </div>
