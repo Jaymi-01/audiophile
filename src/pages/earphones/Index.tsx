ee890eed2d9554ae43fb40cab8b637154539a99e
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-11-02 13:10:30 +0100

    started implementing my type to write out the js

--- src/pages/earphones/Index.tsx
+++ src/pages/earphones/Index.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Header from './Header.jsx'
-import NewProduct from './NewProduct.jsx'
-import Gadgets from '../home/Gadgets.jsx'
-import Footer from '../home/Footer.jsx'
+import React from "react";
+import Header from "./Header";
+import NewProduct from "./NewProduct";
+import Gadgets from "../home/Gadgets";
+import Footer from "../home/Footer";
 
-const Index = () => {
+const Index: React.FC = () => {
   return (
     <div>
-        <Header />
-        <NewProduct />
-        <Gadgets />
-        <Footer />
+      <Header />
+      <NewProduct />
+      <Gadgets />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
