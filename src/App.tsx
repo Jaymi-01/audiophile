b77cb426a0edb8fa1185214588e33e41a4682dd5
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-11-02 17:18:42 +0100

    imported the scroll fix and xx59 headset

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import Header from "./components/Header.jsx";
 import Home from "./pages/home/Index.jsx";
 import Headphones from "./pages/headphones/Index.jsx";
 import Speakers from "./pages/speakers/Index.jsx";
 import Earphones from "./pages/earphones/Index.jsx";
 import XX99MarkII from "./pages/headphones/XX99MarkII.jsx";
+import XX99MarkI from "./pages/headphones/XX99MarkI.js";
+import XX59 from "./pages/headphones/XX59";
 
 const App: React.FC = () => {
   const cartCount = 3;
@@ -13,6 +16,7 @@ const App: React.FC = () => {
     <Router>
       <div className="font-texts">
         <Header cartCount={cartCount} />
+        <ScrollToTop />
         <div className="bg-background app">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -20,6 +24,8 @@ const App: React.FC = () => {
             <Route path="/speakers" element={<Speakers />} />
             <Route path="/earphones" element={<Earphones />} />
             <Route path="/headphones/xx99-mark-ii" element={<XX99MarkII />} />
+            <Route path="/headphones/xx99-mark-i" element={<XX99MarkI />} />
+            <Route path="/headphones/xx59" element={<XX59 />} />
           </Routes>
         </div>
       </div>
